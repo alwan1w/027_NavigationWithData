b466cb2ea2b869b11c3f76862b0249189341a270
Alwan Fauzi <alwan.fauzi.ft21@mail.umy.ac.id>
2023-11-14 20:43:09 +0700

    menambah fun CustomerDetailsScreen

--- app/src/main/java/com/example/pertemuan7/HalamanPelanggan.kt
+++ app/src/main/java/com/example/pertemuan7/HalamanPelanggan.kt
@@ -1,2 +1,9 @@
 package com.example.pertemuan7
 
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CustomerDetailsScreen(
+    onConfirmButtonClicked: (String, String, String) -> Unit,
+    onCancelButtonClicked: () -> Unit
+){}
